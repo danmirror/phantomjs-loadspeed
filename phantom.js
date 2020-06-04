2f3dd705f799b80515f7f8fcde20bd239c7f9469
whiterose666 <danuandreanid@gmail.com>
2020-06-04 12:17:30 +0700

    first commit

---
+++ phantom.js
@@ -0,0 +1,23 @@
+var page = require('webpage').create(),
+    system = require('system'),
+    t, address;
+ 
+if (system.args.length === 1){
+    console.log('Usage: loadspeed.js <some URL>');
+    phantom.exit();
+}
+ 
+t = Date.now();
+address = system.args[1];
+console.log('Start calculating...');
+page.open(address, function(status){
+    if (status !== 'success'){
+        console.log('FAIL to load the address');
+    }
+    else{
+        t = Date.now() - t;
+        console.log('Loading time ' + t + " msec");
+        console.log(address + " was succesfully loaded..");
+    }
+    phantom.exit();
+});
